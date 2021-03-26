c3f3a35093c7b65889ac030942608138736b136e
nwraimo <raimo.nick9@gmail.com>
2021-03-25 19:52:18 -0700

    task 1 complete

--- index.js
+++ index.js
@@ -42,8 +42,24 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(edible){
+      if(this.stomach.length < 10){
+        this.stomach.push(edible);
+      }
+    }
+    poop(){
+      this.stomach = [];
+    }
+    toString(){
+      return `${this.name}, ${this.age}`
+    }
   }
+console.log(Person);  
   
   /*
     TASK 2
